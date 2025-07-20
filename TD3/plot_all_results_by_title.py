b94f34e7b6984d304b001c926ed88650a3fb465a
abdelzaro <abdelzaro8@gmail.com>
2025-07-19 21:02:58 -0400

    ignore thesse results, accidently stopped run

--- TD3/plot_all_results_by_title.py
+++ TD3/plot_all_results_by_title.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # === CONFIGURATION ===
 results_dir = '/home/az/DRL-robot-navigation/TD3/results'  # full path
-file_prefix = 'TD3_velodyne_20250718-121100'  # what to match before the timestamp
+file_prefix = 'TD3_velodyne_run5_2gap_normalized'  # what to match before the timestamp
 
 # === LOAD & FILTER FILES ===
 all_files = [f for f in os.listdir(results_dir)
