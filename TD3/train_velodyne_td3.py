3aeb9ba3a8597dc59644a2fdf006223f3087c0d5
abdelzaro <abdelzaro8@gmail.com>
2025-07-24 18:48:39 -0400

    added robotname to title:

--- TD3/train_velodyne_td3.py
+++ TD3/train_velodyne_td3.py
@@ -265,7 +265,7 @@ parser.add_argument("file_name", type=str, help="Base file name for saving model
 args = parser.parse_args()
 
 robot_name = args.robot_name
-file_name  = args.file_name  # Use  what you passed on the CLI 
+file_name  = f"{args.file_name}_{robot_name}"  # attach robot name at the end
 
 env = GazeboEnv("multi_robot_scenario.launch", environment_dim, robot_name=robot_name)
 time.sleep(5)
