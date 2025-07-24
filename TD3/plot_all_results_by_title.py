33cadc04bcc1501ba770dba0ace8c0d1b6134dec
abdelzaro <abdelzaro8@gmail.com>
2025-07-24 10:01:09 -0400

    ignore that last commit, i got the pcs mixed up. these are negative result,s i put hte plot in the folder

--- TD3/plot_all_results_by_title.py
+++ TD3/plot_all_results_by_title.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # === CONFIGURATION ===
 results_dir = '/home/mini/DRL-robot-navigation/TD3/results/'  # or full path
-file_prefix = 'TD3_velodyne_20250723'  # what to match before the timestamp
+file_prefix = 'TD3_velodyne_20250724-081716'  # what to match before the timestamp
 
 # === LOAD & FILTER FILES ===
 all_files = [f for f in os.listdir(results_dir)
