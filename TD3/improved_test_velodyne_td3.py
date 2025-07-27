32187d38e27dd3ad9454d8ec814bcb0eeaf740c3
abdelzaro <abdelzaro8@gmail.com>
2025-07-27 18:29:51 -0400

    i changed the range of episodes high_reward.py looks at so I can pickup on a run that had a reward of around 30. that last nav data i collected the highest reward in that range was 0

--- TD3/improved_test_velodyne_td3.py
+++ TD3/improved_test_velodyne_td3.py
@@ -98,7 +98,7 @@ episode_num = 0
 
 environment_dim = 20
 robot_dim = 4
-dgap_number_gaps_dim = 7 * 4 # 5 gaps * 4 floats in the dgap_flat_list
+dgap_number_gaps_dim = 10 * 4 # 5 gaps * 4 floats in the dgap_flat_list
 env = GazeboEnv("multi_robot_scenario.launch", environment_dim)
 time.sleep(5)
 torch.manual_seed(seed)
