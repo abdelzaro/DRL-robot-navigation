c672e76e52cc9ac5376d85f4c420aeca39384966
abdelzaro <abdelzaro8@gmail.com>
2025-07-27 08:34:33 -0400

    10 gap poisitive result 60 highest reward 130 episodes

--- TD3/train_velodyne_td3.py
+++ TD3/train_velodyne_td3.py
@@ -257,7 +257,7 @@ if save_model and not os.path.exists("./pytorch_models"):
 use_lidar = False # in the observation
 environment_dim = 20
 robot_dim = 4
-dgap_number_gaps_dim = 7 * 4 # 5 gaps * 4 floats in the dgap_flat_list
+dgap_number_gaps_dim = 10 * 4 # 5 gaps * 4 floats in the dgap_flat_list
 env = GazeboEnv("multi_robot_scenario.launch", environment_dim)
 time.sleep(5)
 torch.manual_seed(seed)
--- TD3/velodyne_env.py
+++ TD3/velodyne_env.py
@@ -116,10 +116,10 @@ class GazeboEnv:
             self.gaps.append(
                 [self.gaps[m][1], self.gaps[m][1] + np.pi / self.environment_dim]
             )
-        self.gaps[-1][-1] += 0.03 # Abdel: this is part of the DRL code not the dgap code
+        self.gaps[-1][-1] += 0.03 # Abdel: this is part of the DRL code not the dgap  code
         
         # self.dgap_flat_vector = []# this is part of the dgap code
-        self.dgap_flat_vector = np.zeros(28, dtype=np.float32)
+        self.dgap_flat_vector = np.zeros(40, dtype=np.float32)
 
         port = "11311"
         subprocess.Popen(["roscore", "-p", port])
@@ -181,7 +181,7 @@ class GazeboEnv:
 
     def gaps_callback(self, msg):
         gaps_flat = []
-        max_gaps = 7  #IF YOU CHANGE THIS!!!:  update dgap_number_gaps_dim in train_velodyne_td3.py
+        max_gaps = 10  #IF YOU CHANGE THIS!!!:  update dgap_number_gaps_dim in train_velodyne_td3.py
         # and dgap_flat_vector initial value
         max_range = 5.0  
         
